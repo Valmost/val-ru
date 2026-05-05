9343bcf06b9e66642a54a5ff0380e68974dfdac9
Valmost <73470239+Valmost@users.noreply.github.com>
2026-05-05 16:44:10 +0500

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -1,11 +1,11 @@
 import requests
 
-r = requests.post('http://127.0.0.1:5000/api/token',
+r = requests.post('http://valmost.site/api/token',
                   json={'username': 'Valmost3377@mail.ru', 'password': '123'})
 token = r.json()['token']
 
-with open('C:/Users/Иван/Documents/low_poly1.pdf', 'rb') as f:
-    r = requests.post('http://localhost:5000/api/process',
+with open('input.pdf', 'rb') as f:
+    r = requests.post('http://valmost.site/api/process',
                       headers={'Authorization': f'Bearer {token}'},
                       files={'file': f},
                       data={'width': 500, 'height': 500, 'algorithm': 'shelf'})
